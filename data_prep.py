7733568cd97698bae2998a13ee15aed3719a1bf7
Kalvin Chang <kalvin1204@gmail.com>
2024-06-21 00:12:17 -0400

    feat: load voxangeles phones into a TextGrid

---
+++ data_prep.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from textgrids import TextGrid
+from tqdm import tqdm
+import argparse
+
+
+def _get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=Path, help="Path to dataset")
+    parser.add_argument("--textgrid_path", type=Path, help="Path to TextGrids")
+    parser.add_argument("--dataset_type", type=str, choices=list(_SUPPORTED_DATASETS.keys()))
+    parser.add_argument("--output_path", type=Path, help="Output csv folder")
+
+    return parser.parse_args()
+
+
+def _voxangeles(dataset_path: Path, textgrid_path: Path):
+    rows = []
+    # each file is one word
+    for p in tqdm(textgrid_path.glob("*/*.TextGrid")):
+        grid = TextGrid(p)
+        phone_key = "phone" if "phone" in grid else ("phones" if "phones" in grid else "Narrow")
+        for phone in grid[phone_key]:
+            if phone is not None:
+                rows.append({
+                    "text": phone.text, # phone
+                    "start": phone.xmin,
+                    "finish": phone.xmax,
+                    "path": str((dataset_path / p.relative_to(p.parents[3]).with_suffix(".flac")).absolute()),
+                })
+
+    return pd.DataFrame(rows)
+
+
+_SUPPORTED_DATASETS = {
+    "voxangeles": _voxangeles,
+}
+
+
+if __name__ == "__main__":
+    args = _get_args()
+    parser = _SUPPORTED_DATASETS[args.dataset_type]
+    df = parser(dataset_path=args.dataset_path, textgrid_path=args.textgrid_path)
+    df.to_pickle((args.output_path / args.dataset_type).with_suffix('.pkl'))
---
+++ voxangeles.sh
@@ -0,0 +1,6 @@
+mkdir data
+cd data
+git clone https://github.com/pacscilab/voxangeles/
+cd voxangeles/data/audited_aligned/
+unzip \*.zip
+rm -rf __MACOSX

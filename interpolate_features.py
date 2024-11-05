1fdd24dd49f15504bdfba57aa0fbbd76aa6a3986
Kalvin Chang <kalvin1204@gmail.com>
2024-11-04 21:05:07 -0500

    feat: synthesize and reconstruct single vowels

--- interpolate_features.py
+++ interpolate_features.py
@@ -15,38 +15,55 @@ import scipy
 # Method: Optimize input signal to reproduce SSL features for the signals for 2 vowels
 # Hypothesis: The proximity of the cluster will reproduce [a] and [i].
 
+def reconstruct_signal(net, gt_signal, file_name):
+    gt_feats = net.feature_extractor(gt_signal[None, :])
+    gt_feats = gt_feats.transpose(1, 2)
+    _, gt_feats = net.feature_projection(gt_feats)
+
+    signal = torch.nn.Parameter(torch.FloatTensor([0.0] * 1600), requires_grad=True)
+    optimizer = torch.optim.Adam([signal])
+
+    acc_loss = []
+    loop = tqdm(range(1000))
+    for it in loop:
+        optimizer.zero_grad()
+
+        feats = net.feature_extractor(signal[None, :])
+        feats = feats.transpose(1, 2)
+        _, feats = net.feature_projection(feats)
+
+        # L2 loss
+        loss = torch.square(feats - gt_feats).mean()
+        acc_loss.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        loop.set_description(f"loss: {loss.item():.4f}")
+
+    # visual verification - did we reconstruct the signal?
+    plt.plot(signal.detach().numpy())
+    plt.savefig(f'plots/{file_name}.png')
+    plt.clf()
 
 
-# WavLM normalizes the magnitude and bias of the signal
+if __name__ == "__main__":
+    # WavLM normalizes the magnitude and bias of the signal
     # average magnitude following a Gaussian
-net = AutoModel.from_pretrained("microsoft/wavlm-large").to("cpu")
-for p in net.parameters():
-    p.requires_grad_(False)
+    net = AutoModel.from_pretrained("microsoft/wavlm-large").to("cpu")
+    for p in net.parameters():
+        p.requires_grad_(False)
 
-# we're synthesizing a vowel (F0 100 Hz)
-gt_signal = get_signal(100)[:1600]
-gt_feats = net.feature_extractor(gt_signal[None, :])
-gt_feats = gt_feats.transpose(1, 2)
-_, gt_feats = net.feature_projection(gt_feats)
+    # given summation of 3 sine signals, can we reconstruct the sine signal?
+    vowels = zip(
+        'ieɛaɑʌɤɯyøœɶɒɔou',
+        [240, 390, 610, 850, 750, 600, 460, 300, 235, 370, 585, 820, 700, 500, 360, 250],
+        [2400, 2300, 1900, 1610, 940, 1170, 1310, 1390, 2100, 1900, 1710, 1530, 750, 700, 640, 595],
+    )
+    for vowel, f1, f2 in vowels:
+        # TODO: magnitude - empirically determine
+        # we're synthesizing a vowel (F0 100 Hz)
+        vowel_signal = get_signal([f1, f2])[:1600]
+        # TODO: F3
+        reconstruct_signal(net, vowel_signal, 'single_vowel_' + vowel)
 
-signal = torch.nn.Parameter(torch.FloatTensor([0.0] * 1600), requires_grad=True)
-optimizer = torch.optim.Adam([signal])
-
-acc_loss = []
-loop = tqdm(range(1000))
-for it in loop:
-    optimizer.zero_grad()
-
-    feats = net.feature_extractor(signal[None, :])
-    feats = feats.transpose(1, 2)
-    _, feats = net.feature_projection(feats)
-
-    # L2 loss
-    loss = torch.square(feats - gt_feats).mean()
-    acc_loss.append(loss.item())
-    loss.backward()
-    optimizer.step()
-    loop.set_description(f"loss: {loss.item():.4f}")
-
-# visual verification
-plt.plot(signal.detach().numpy())
+    # TODO: check the intermediate representations
+        # linear interpolation of the features of [i] and [u]
